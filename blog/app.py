ff22a73142ce9d8f8b40a76bd48d403e2c78c4de
mbackReserved <lowmustgoon@gmail.com>
2023-03-04 11:58:59 +0300

    register blueprint with user_list and user_details

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template
-
+from blog.views.users import users_app
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+app.register_blueprint(users_app, url_prefix="/users")
--- blog/views/users.py
+++ blog/views/users.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
 
 users_app = Blueprint("users_app", __name__)
 USERS = {
@@ -10,3 +11,12 @@ USERS = {
 @users_app.route("/")
 def users_list():
     return render_template("users/list.html", users=USERS)
+
+
+@users_app.route("/<int:user_id>/")
+def user_details(user_id: int):
+    try:
+        user_name = USERS[user_id]
+    except KeyError:
+        raise NotFound(f"User {user_id} doesn't exist!")
+    return render_template('users/details.html', user_id=user_id, user_name = user_name)
