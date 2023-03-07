b2d35ede5c6f4f4c07e15c718c1f5f9b6eafc789
mbackReserved <lowmustgoon@gmail.com>
2023-03-07 07:26:21 +0300

    init db & create users

--- blog/app.py
+++ blog/app.py
@@ -15,3 +15,22 @@ app.register_blueprint(articles_app, url_prefix="/articles")
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/blog.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    from blog.models import User
+    admin = User(username='admin', is_staff=True)
+    vadim = User(username="vadim")
+
+    db.session.add(admin)
+    db.session.add(vadim)
+    db.session.commit()
+
+    print("created users:", admin, vadim)
