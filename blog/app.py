fd6d387fa16c3c7d2f0de4d576477f7d92154b5e
mbackReserved <lowmustgoon@gmail.com>
2023-03-07 07:02:27 +0300

    create default User model

--- blog/app.py
+++ blog/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models.database import db
 
 app = Flask(__name__)
 
@@ -10,3 +11,7 @@ def index():
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/blog.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
