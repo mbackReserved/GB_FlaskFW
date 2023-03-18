ae3bc6911a3b0cae622ce2f877c1e35ea735df03
mbackReserved <lowmustgoon@gmail.com>
2023-03-18 12:53:52 +0300

    class configs

--- blog/app.py
+++ blog/app.py
@@ -19,6 +19,9 @@ app.config["SECRET_KEY"] = "abcdefg123456"
 app.register_blueprint(auth_app, url_prefix="/auth")
 login_manager.init_app(app)
 
+cfg_name = os.environ.get("CONFIG_NAME") or "ProductionConfig"
+app.config.from_object(f"blog.configs.{cfg_name}")
+
 
 @app.route('/')
 def index():
