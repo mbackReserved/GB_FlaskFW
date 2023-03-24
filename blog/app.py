2761d809bc5fb662d9e0bebb6591a5d4ef5cc7fb
mbackReserved <lowmustgoon@gmail.com>
2023-03-24 07:59:22 +0300

    create authors model

--- blog/app.py
+++ blog/app.py
@@ -6,6 +6,7 @@ from blog.views.auth import login_manager, auth_app
 import os
 from flask_migrate import Migrate
 from blog.security import flask_bcrypt
+from blog.views.authors import authors_app
 
 app = Flask(__name__)
 
@@ -46,3 +47,6 @@ def create_admin():
     db.session.commit()
     
     print("created admin:", admin)
+
+
+app.register_blueprint(authors_app, url_prefix="/authors")
