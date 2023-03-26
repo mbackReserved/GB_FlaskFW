a77f14b20a6c0d15650fded543285319fb76d490
mbackReserved <lowmustgoon@gmail.com>
2023-03-26 08:41:28 +0300

    limit access to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,18 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from blog import models
 from blog.models.database import db
+from flask import redirect, url_for
+from flask_login import current_user
+
 
 # Customized admin interface
 class CustomView(ModelView):
-    pass
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_staff
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for("auth_app.login"))
 
 class TagAdminView(CustomView):
     column_searchable_list = ("name",)
@@ -25,8 +32,23 @@ class UserAdminView(CustomView):
     can_delete = False
 
 
-# Create admin with custom base template
-admin = Admin(name="Blog Admin", template_mode="bootstrap4")
+
+
+class MyAdminIndexView(AdminIndexView):
+    
+    @expose("/")
+    def index(self):
+        if not (current_user.is_authenticated and current_user.is_staff):
+            return redirect(url_for("auth_app.login"))
+        return super(MyAdminIndexView, self).index()
+
+
+# Create admin with custom props
+admin = Admin(
+    name="Blog Admin",
+    index_view=MyAdminIndexView(),
+    template_mode="bootstrap4",
+)
 
 # Add views
 admin.add_view(CustomView(models.Article, db.session, category="Models"))
