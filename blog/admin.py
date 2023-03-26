fb51ce93fa5cd03e8f4ca1395967ba6018d933ea
mbackReserved <lowmustgoon@gmail.com>
2023-03-26 08:33:51 +0300

    create admin views for models

--- blog/admin.py
+++ blog/admin.py
@@ -7,9 +7,28 @@ from blog.models.database import db
 class CustomView(ModelView):
     pass
 
+class TagAdminView(CustomView):
+    column_searchable_list = ("name",)
+    column_filters = ("name",)
+    can_export = True
+    export_types = ["csv", "xlsx"]
+    create_modal = True
+    edit_modal = True
+
+class UserAdminView(CustomView):
+    column_exclude_list = ("_password",)
+    column_searchable_list = ("first_name", "last_name", "username", "is_staff", "email")
+    column_filters = ("first_name", "last_name", "username", "is_staff", "email")
+    column_editable_list = ("first_name", "last_name", "is_staff")
+    can_create = True
+    can_edit = True
+    can_delete = False
+
+
 # Create admin with custom base template
 admin = Admin(name="Blog Admin", template_mode="bootstrap4")
 
 # Add views
-admin.add_view(CustomView(models.Tag, db.session, category="Models"))
 admin.add_view(CustomView(models.Article, db.session, category="Models"))
+admin.add_view(TagAdminView(models.Tag, db.session, category="Models"))
+admin.add_view(UserAdminView(models.User, db.session, category="Models"))
