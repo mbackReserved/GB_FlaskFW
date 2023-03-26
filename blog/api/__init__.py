c4b610fb35b52063d6ab462e0c5e8cf7d336e12b
mbackReserved <lowmustgoon@gmail.com>
2023-03-26 09:28:25 +0300

    register all views

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,5 +1,8 @@
 from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
+from blog.api.user import UserList, UserDetail
+from blog.api.author import AuthorList, AuthorDetail
+from blog.api.article import ArticleList, ArticleDetail
 from combojsonapi.spec import ApiSpecPlugin
 
 
@@ -10,6 +13,9 @@ def create_api_spec_plugin(app):
         # so API gets organized into groups. it's optional.
         tags={
             "Tag": "Tag API",
+            "User": "User API",
+            "Author": "Author API",
+            "Article": "Article API",
         }
     )
     return api_spec_plugin
@@ -24,6 +30,10 @@ def init_api(app):
         ],
     )
 
-    
+
     api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
     api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
+    api.route(UserList, "user_list", "/api/users/", tag="User")
+    api.route(UserDetail, "user_detail", "/api/users/<int:id>/", tag="User")
+    api.route(AuthorList, "author_list", "/api/authors/", tag="Author")
+    api.route(AuthorDetail, "author_detail", "/api/authors/<int:id>/", tag="Author")
