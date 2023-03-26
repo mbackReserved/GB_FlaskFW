f2ef3bebbc9b3e059c5d5b86e17e6f902aa1af7c
mbackReserved <lowmustgoon@gmail.com>
2023-03-26 09:18:38 +0300

    init comboJSONAPI, ApiSpec, PyYAML

--- blog/api/__init__.py
+++ blog/api/__init__.py
@@ -1,11 +1,29 @@
 from flask_combo_jsonapi import Api
 from blog.api.tag import TagList, TagDetail
+from combojsonapi.spec import ApiSpecPlugin
+
+
+def create_api_spec_plugin(app):
+    api_spec_plugin = ApiSpecPlugin(
+        app=app,
+        # Declaring tags list with their descriptions,
+        # so API gets organized into groups. it's optional.
+        tags={
+            "Tag": "Tag API",
+        }
+    )
+    return api_spec_plugin
 
 
 def init_api(app):
-    api = Api(app)
+    api_spec_plugin = create_api_spec_plugin(app)
+    api = Api(
+        app,
+        plugins=[
+            api_spec_plugin,
+        ],
+    )
 
-    api.route(TagList, "tag_list", "/api/tags/")
-    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/")
     
-    return api
+    api.route(TagList, "tag_list", "/api/tags/", tag="Tag")
+    api.route(TagDetail, "tag_detail", "/api/tags/<int:id>/", tag="Tag")
--- blog/configs.py
+++ blog/configs.py
@@ -14,6 +14,9 @@ class DevConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     FLASK_ADMIN_SWATCH = 'cosmo'
+    OPENAPI_URL_PREFIX = '/api/swagger'
+    OPENAPI_SWAGGER_UI_PATH = '/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
 
 
 class TestingConfig(BaseConfig):
